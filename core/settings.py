15691a61d28548d6ca079f2dd499cafbc83aa084
Inam Ullah <inamullahkhan7462@gmail.com>
2026-04-11 17:32:12 +0500

    Fix version mismatch and open allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-ihju3!j*sy7eb=dpr%-827h$dt_^zbjxa(yhvpte6ljed)21rf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['silentspeak-backend.onrender.com', 'localhost', '127.0.0.1']
-
+# Change this line
+ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
